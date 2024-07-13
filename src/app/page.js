ff8f2a0f7fc0cfc5890d96f46ddd009427b474b4
klenilsonrox <klenilsoncoc@gmail.com>
2024-07-13 08:57:24 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,71 @@
-import Image from "next/image";
+'use client'
+import React, { useEffect, useRef, useState } from 'react';
 
-export default function Home() {
+const Page = () => {
+  const [modalImage, setOpenModalImage] = useState(false);
+  const imageRef = useRef();
+  const [id, setId] = useState(1);
+  const [image, setImage] = useState("");
+
+  const images = [
+    { image: "../images/1.jpg", id: 1 },
+    { image: "../images/2.jpg", id: 2 },
+    { image: "../images/3.jpg", id: 3 },
+    { image: "../images/4.jpg", id: 4 },
+    { image: "../images/5.jpg", id: 5 },
+  ];
+
+  useEffect(() => {
+    setImage("../images/1.jpg");
+  }, []);
+
+  function verImage(e) {
+    e.preventDefault();
+    const imgId = Number(e.target.id);
+    setOpenModalImage(true);
+    setId(imgId);
+    setImage(images.find(img => img.id === imgId).image);
+  }
+
+  function aumentarId() {
+    const nextId = id < images.length ? id + 1 : 1;
+    setId(nextId);
+    setImage(images.find(img => img.id === nextId).image);
+  }
+
+  function diminuirId() {
+    const prevId = id > 1 ? id - 1 : images.length;
+    setId(prevId);
+    setImage(images.find(img => img.id === prevId).image);
+  }
+
+  function closeModal(e) {
+    if (e.target.id === "modal") {
+      setOpenModalImage(false);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='p-4'>
+      <div className='flex gap-4 max-w-7xl mx-auto w-full flex-wrap justify-center mt-4'>
+        {images.map(img => (
+          <img key={img.id} src={img.image} alt="" className='w-full max-w-[300px] cursor-pointer' onClick={verImage} id={img.id} />
+        ))}
+      </div>
+      {modalImage && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm" id='modal' onClick={closeModal}>
+          <button className='absolute top-4 right-4 text-white text-2xl' onClick={() => setOpenModalImage(false)}>✖</button>
+          <div className='flex items-center gap-3 w-full max-w-3xl px-4'>
+            <p className='cursor-pointer text-2xl  font-bold bg-white text-black flex items-center justify-center rounded-full w-10 h-10 relative right-[-30px]' onClick={diminuirId}>←</p>
+            <div className="flex justify-center w-full">
+              <img src={image} alt="" className='w-full max-h-screen object-contain rounded-lg' ref={imageRef} />
+            </div>
+            <p className='cursor-pointer text-2xl  font-bold bg-white text-black flex items-center justify-center rounded-full w-10 h-10 relative right-[30px]' onClick={aumentarId}>→</p>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      )}
+    </div>
   );
-}
+};
+
+export default Page;
